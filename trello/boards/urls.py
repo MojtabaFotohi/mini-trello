8bb4484de9afaf668e8906ab523ac81c391e732c
MojtabaFotohi <mojtaba.fotohi.official@gmail.com>
2025-09-18 17:52:35 +0330

    Test and verify Board CRUD APIs

--- trello/boards/urls.py
+++ trello/boards/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import BoardListCreateView, BoardDetailView
 
 urlpatterns = [
-        path('', BoardListCreateView.as_view(), name='board-list-create'),
-        path('<int:pk>/', BoardDetailView.as_view(), name='board-detail'),
+path('', BoardListCreateView.as_view(), name='board-list-create'),
+path('<int:pk>/', BoardDetailView.as_view(), name='board-detail'),
 ]
