36bfcd1791bb8336c7868c655d34959380bf886c
MojtabaFotohi <mojtaba.fotohi.official@gmail.com>
2025-09-21 19:39:34 +0330

    add swagger api docs

--- trello/trello/urls.py
+++ trello/trello/urls.py
@@ -1,9 +1,34 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.conf.urls.i18n import i18n_patterns
 from users.views import CustomTokenObtainPairView
+
+#api docs
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Modern Trello API",
+        default_version='v1',
+        description="API documentation for Modern Trello project",
+        terms_of_service="https://www.example.com/terms/",
+        contact=openapi.Contact(email="contact@example.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
+
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,6 +40,10 @@ urlpatterns = [
     path('lists/', include('lists.urls')),
     path('invitations/', include('invitations.urls')),
     path('', TemplateView.as_view(template_name='index.html'), name='home'),
+    
+    # Swagger UI
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 
@@ -22,3 +51,10 @@ urlpatterns += i18n_patterns(
     path('', TemplateView.as_view(template_name='index.html'), name='home'),
     prefix_default_language=False,  
 )
+
+
+
+
+
+
+
